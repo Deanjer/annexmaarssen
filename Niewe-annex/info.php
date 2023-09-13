83749b8d1f2b41883eac5bcc41061fc6b8e7694e
Deanjer <220509@student.glu.nl>
2023-09-13 14:00:07 +0200

    infopagina3

--- Niewe-annex/info.php
+++ Niewe-annex/info.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/info.css">
 </head>
+
 <body>
-    
+    <div class="tst"></div>
+    <div class="info-main">
+        <div class="info-main-container">
+            <div class="info-main-top">
+                <div class="info-main-top-title"></div>
+            </div>
+            <div class="info-main-middle">
+                <div class="info-main-middle-left">
+                    <img src="./openheimer.jpg" alt="">
+                    test
+                </div>
+                <div class="info-main-middle-right">test</div>
+            </div>
+            <div class="info-main-bottom"></div>
+        </div>
+    </div>
+  
 </body>
+
 </html>
