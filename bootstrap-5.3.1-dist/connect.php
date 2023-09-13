e9e4d9bc6f0b3e0995cae881c63b57f574f8b60b
Neochees <113420296+Neochees@users.noreply.github.com>
2023-09-13 11:12:24 +0200

    New-Header

--- bootstrap-5.3.1-dist/connect.php
+++ bootstrap-5.3.1-dist/connect.php
@@ -7,42 +7,29 @@
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg" style="background-color: #9E2629;">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="./homepage.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+<nav class="navbar bg-body-tertiary d-flex">
+  <div class="container mx-auto p-2">
+    <a class="navbar-brand p-2 flex-grow-1" href="./homepage.php"><img src="./img/logo_hoofd.png" alt="Bootstrap" width="150" height="50"></a>
+    <a class="nav-link p-2" href="./agenda.php">FILM AGENDA</a>
+    <a class="nav-link p-2" href="#">ALLE VESTINGEN</a>
+    <a class="nav-link p-2" href="./connect.php">CONTACT</a>
   </div>
 </nav>
+<div class="text-bg-danger p-3 d-flex "><span class="fs-4 fw-bold p-2">KOOP JE TICKETS</span>
+<div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle p-2" data-bs-toggle="dropdown" aria-expanded="false">
+    Kies je film
+  </button>
+  <ul class="dropdown-menu p-2">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Separated link</a></li>
+  </ul>
+</div>
+<button type="button" class="btn btn-light btn btn-outline-danger">Bestel tickets</button>
+</div>
 <div class="text-bg-dark p-3"><H1>Hello world</H1></div>
     <script src="js/bootstrap.bundle.js"></script>
 </body>
