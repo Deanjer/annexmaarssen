77a1178e467a1b0e8d45c8f2a38be85df141fbfd
Bram Steur <220507@student.glu.nl>
2023-09-18 11:02:31 +0200

    agenda

--- Niewe-annex/agenda.php
+++ Niewe-annex/agenda.php
@@ -10,10 +10,169 @@
     <div>
         <h1 id="title">FILM AGENDA</h1>
     </div>
-    <div class="film">
-        <div class="slot"></div>
-        <img class="poster" src="./img/oppenheimerposter.jpg" alt="oppenheimer">
-        <h3 class="name" id="oppenheimer">Oppenheimer</h3>
-    </div>
-</body>
+    <div id="agenda">
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" src="./img/oppenheimerposter.jpg" alt="oppenheimer">
+            <div class="movietitle"><h3 class="name">Oppenheimer</h3></div>
+            <h5 class="desciption" id="oppenheimer">THE WORLD FOREVER CHANGES.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="barbieposter" src="./img/barbieposter.jpg" alt="barbie">
+            <div class="movietitle"><h3 class="name">Barbie</h3></div>
+            <h5 class="desciption" id="barbie">SHE’S EVERYTHING. HE’S JUST KEN.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="talktomeposter" src="./img/talktomeposter.jpg" alt="talktome">
+            <div class="movietitle"><h3 class="name">Talk To Me</h3></div>
+            <h5 class="desciption" id="talktome">YOU CALL. THEY’LL ANSWER.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="tmntposter" src="./img/tmntposter.jpg" alt="tmnt">
+            <div class="movietitle"><h3 class="name" id="tmntname">Teenage Mutant Ninja Turtles: Mutant Mayhem</h3></div>
+            <h5 class="desciption" id="tmnt">A NEW GENERATION OF HEROES WILL RISE… STRAIGHT FROM THE SEWERS.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" src="./img/oppenheimerposter.jpg" alt="oppenheimer">
+            <div class="movietitle"><h3 class="name">Oppenheimer</h3></div>
+            <h5 class="desciption" id="oppenheimer">THE WORLD FOREVER CHANGES.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="talktomeposter" src="./img/talktomeposter.jpg" alt="talktome">
+            <div class="movietitle"><h3 class="name">Talk To Me</h3></div>
+            <h5 class="desciption" id="talktome">YOU CALL. THEY’LL ANSWER.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="granturismoposter" src="./img/granturismoposter.jpg" alt="granturismo">
+            <div class="movietitle"><h3 class="name">Gran Tursimo</h3></div>
+            <h5 class="desciption" id="granturismo">FROM GAMER TO RACER.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="barbieposter" src="./img/barbieposter.jpg" alt="barbie">
+            <div class="movietitle"><h3 class="name">Barbie</h3></div>
+            <h5 class="desciption" id="barbie">SHE’S EVERYTHING. HE’S JUST KEN.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="tmntposter" src="./img/tmntposter.jpg" alt="tmnt">
+            <div class="movietitle"><h3 class="name" id="tmntname">Teenage Mutant Ninja Turtles: Mutant Mayhem</h3></div>
+            <h5 class="desciption" id="tmnt">A NEW GENERATION OF HEROES WILL RISE… STRAIGHT FROM THE SEWERS.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="bluebeetleposter" src="./img/bluebeetleposter.jpg" alt="bluebeetle">
+            <div class="movietitle"><h3 class="name" id="bluebeetlenname">Blue Beetle</h3></div>
+            <h5 class="desciption" id="bluebeetle">JAIME REYES IS A SUPERHERO WHETHER HE LIKES IT OR NOT.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" src="./img/oppenheimerposter.jpg" alt="oppenheimer">
+            <div class="movietitle"><h3 class="name">Oppenheimer</h3></div>
+            <h5 class="desciption" id="oppenheimer">THE WORLD FOREVER CHANGES.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="granturismoposter" src="./img/granturismoposter.jpg" alt="granturismo">
+            <div class="movietitle"><h3 class="name">Gran Tursimo</h3></div>
+            <h5 class="desciption" id="granturismo">FROM GAMER TO RACER.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="talktomeposter" src="./img/talktomeposter.jpg" alt="talktome">
+            <div class="movietitle"><h3 class="name">Talk To Me</h3></div>
+            <h5 class="desciption" id="talktome">YOU CALL. THEY’LL ANSWER.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="tmntposter" src="./img/tmntposter.jpg" alt="tmnt">
+            <div class="movietitle"><h3 class="name" id="tmntname">Teenage Mutant Ninja Turtles: Mutant Mayhem</h3></div>
+            <h5 class="desciption" id="tmnt">A NEW GENERATION OF HEROES WILL RISE… STRAIGHT FROM THE SEWERS.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="barbieposter" src="./img/barbieposter.jpg" alt="barbie">
+            <div class="movietitle"><h3 class="name">Barbie</h3></div>
+            <h5 class="desciption" id="barbie">SHE’S EVERYTHING. HE’S JUST KEN.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="straysposter" src="./img/straysposter.jpg" alt="strays">
+            <div class="movietitle"><h3 class="name">Strays</h3></div>
+            <h5 class="desciption" id="strays">GO FETCH YOURSELF.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="bluebeetleposter" src="./img/bluebeetleposter.jpg" alt="bluebeetle">
+            <div class="movietitle"><h3 class="name" id="bluebeetlenname">Blue Beetle</h3></div>
+            <h5 class="desciption" id="bluebeetle">JAIME REYES IS A SUPERHERO WHETHER HE LIKES IT OR NOT.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" src="./img/oppenheimerposter.jpg" alt="oppenheimer">
+            <div class="movietitle"><h3 class="name">Oppenheimer</h3></div>
+            <h5 class="desciption" id="oppenheimer">THE WORLD FOREVER CHANGES.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="granturismoposter" src="./img/granturismoposter.jpg" alt="granturismo">
+            <div class="movietitle"><h3 class="name">Gran Tursimo</h3></div>
+            <h5 class="desciption" id="granturismo">FROM GAMER TO RACER.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="nunposter" src="./img/nun2poster.jpg" alt="nun">
+            <div class="movietitle"><h3 class="name">The Nun II</h3></div>
+            <h5 class="desciption" id="thenun">THE GREATEST EVIL IN THE CONJURING UNIVERSE.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="barbieposter" src="./img/barbieposter.jpg" alt="barbie">
+            <div class="movietitle"><h3 class="name">Barbie</h3></div>
+            <h5 class="desciption" id="barbie">SHE’S EVERYTHING. HE’S JUST KEN.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="talktomeposter" src="./img/talktomeposter.jpg" alt="talktome">
+            <div class="movietitle"><h3 class="name">Talk To Me</h3></div>
+            <h5 class="desciption" id="talktome">YOU CALL. THEY’LL ANSWER.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="tmntposter" src="./img/tmntposter.jpg" alt="tmnt">
+            <div class="movietitle"><h3 class="name" id="tmntname">Teenage Mutant Ninja Turtles: Mutant Mayhem</h3></div>
+            <h5 class="desciption" id="tmnt">A NEW GENERATION OF HEROES WILL RISE… STRAIGHT FROM THE SEWERS.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="straysposter" src="./img/straysposter.jpg" alt="strays">
+            <div class="movietitle"><h3 class="name">Strays</h3></div>
+            <h5 class="desciption" id="strays">GO FETCH YOURSELF.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="bluebeetleposter" src="./img/bluebeetleposter.jpg" alt="bluebeetle">
+            <div class="movietitle"><h3 class="name" id="bluebeetlenname">Blue Beetle</h3></div>
+            <h5 class="desciption" id="bluebeetle">JAIME REYES IS A SUPERHERO WHETHER HE LIKES IT OR NOT.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" src="./img/oppenheimerposter.jpg" alt="oppenheimer">
+            <div class="movietitle"><h3 class="name">Oppenheimer</h3></div>
+            <h5 class="desciption" id="oppenheimer">THE WORLD FOREVER CHANGES.</h5>
+        </div>
+        <div class="film">
+            <div class="slot"></div>
+            <img class="poster" id="granturismoposter" src="./img/granturismoposter.jpg" alt="granturismo">
+            <div class="movietitle"><h3 class="name">Gran Tursimo</h3></div>
+            <h5 class="desciption" id="granturismo">FROM GAMER TO RACER.</h5>
+        </div>
+     </div>
+    </body>
 </html>
