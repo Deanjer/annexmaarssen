0c87a897d1159e7c212c64c735a4b3833a73829e
Bram Steur <220507@student.glu.nl>
2023-09-20 15:24:05 +0200

    database

---
+++ Niewe-annex/database.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ticketing";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO `klanten` (`Voornaam`, `Achternaam`, `email`)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+// $sql = "INSERT INTO `klanten` (`id`, `Voornaam`, `Achternaam`, `email`)
+//  VALUES (NULL, 'Bram', 'Steur', 'bramsteur05@gmail.com')";
+
+$sql = "INSERT INTO `resevering` (`id`, `datum/tijdstip`, `kland_id`, `ticket_kind`, `ticket_normaal`, `ticket_65+`, `voucher_code`, `stoelen`) 
+VALUES (NULL, '2023-07-21 13:59:24.000000', '1', '4', '1', '1', '', '')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
