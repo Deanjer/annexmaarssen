35dd0f110a48694f77f98c13e9aa27a5f2b91216
Neochees <113420296+Neochees@users.noreply.github.com>
2023-09-25 10:34:37 +0200

    a ton

--- Niewe-annex/header.php
+++ Niewe-annex/header.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>header</title>
-    <link rel="stylesheet" href="./css/header.css">
-</head>
-<body>
-    <div id="header">terhy</div>
-</body>
-</html>
+<link rel="stylesheet" href="./css/header.css">
+<header>
+    <div id="logo"><img src="./img/logos.png"></div>
+    <div class="blank"></div>
+    <div id="filmagenda"><a href="./agenda.php">FILM AGENDA</a></div>
+    <div id="vesteginen">ALLE VESTEGINEN</div>
+    <div id="contact">CONTACT</div><br>
+    <div id="tickets">
+        <div class="blank tikcetblank"></div>
+        <h2>KOOP JE TICKETS</h2>
+        <div class="dropdown">
+            <button class="dropbtn">KIES JE FILM</button>
+            <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </div>
+        <div id="bestelbutton"><input type="button"></div>
+    </div>
+</header>
